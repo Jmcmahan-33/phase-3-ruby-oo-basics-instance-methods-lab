bbe03dd2e55cae7da282fc20e228b773d03b73b7
Jared McMahan <jaredm788@gmail.com>
2023-01-08 12:43:44 -0700

    commit tree 90c3072eab07c62b6d82ef5ff9d2c2ffe28acac4

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+    # Class body
+  
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting"
+    end
+  end
+
+  fido = Dog.new
+  fido.bark
+
+  snoopy = Dog.new
+  snoopy.bark
+
+  fido = Dog.new
+  fido.sit
+
+  snoopy = Dog.new
+  snoopy.sit
